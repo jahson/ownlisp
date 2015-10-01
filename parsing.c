993aadd336bd8471c43aee2400c23c3082c01857
jahson <jjahson@gmail.com>
2015-10-01 15:19:47 +0800

    Add decimal type.
    Also defined some awful type conversion. Result will be of first operand type.

--- parsing.c
+++ parsing.c
@@ -2,33 +2,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include <editline/readline.h>
 
 typedef struct {
     int type;
     union {
-        long num;
-        int err;
-    } value;
+        long integer;
+        double decimal;
+        int error;
+    } val;
 } lval;
 
 // possible lval types
-enum { LVAL_NUM, LVAL_ERR };
+enum { LVAL_INTEGER, LVAL_DECIMAL, LVAL_ERROR };
 // possible error types
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
-lval lval_num(long x) {
+lval lval_integer(long x) {
     lval v;
-    v.type = LVAL_NUM;
-    v.value.num = x;
+    v.type = LVAL_INTEGER;
+    v.val.integer = x;
+    return v;
+}
+
+lval lval_decimal(double x) {
+    lval v;
+    v.type = LVAL_DECIMAL;
+    v.val.decimal = x;
     return v;
 }
 
 lval lval_err(int x) {
     lval v;
-    v.type = LVAL_ERR;
-    v.value.err = x;
+    v.type = LVAL_ERROR;
+    v.val.error = x;
     return v;
 }
 
@@ -50,11 +59,14 @@ void print_error(int error_code) {
 
 void lval_print(lval v) {
     switch (v.type) {
-        case LVAL_NUM:
-            printf("%li", v.value.num);
+        case LVAL_INTEGER:
+            printf("%li", v.val.integer);
             break;
-        case LVAL_ERR:
-            print_error(v.value.err);
+        case LVAL_DECIMAL:
+            printf("%f", v.val.decimal);
+            break;
+        case LVAL_ERROR:
+            print_error(v.val.error);
             break;
     }
 }
@@ -66,55 +78,142 @@ void lval_println(lval v) {
 
 lval eval_unary_op(char* operator, lval x) {
     if (strcmp(operator, "-") == 0) {
-        return lval_num(-x.value.num);
+        switch(x.type) {
+            case LVAL_INTEGER:
+                return lval_integer(-x.val.integer);
+                break;
+            case LVAL_DECIMAL:
+                return lval_decimal(-x.val.decimal);
+                break;
+        }
     }
     return lval_err(LERR_BAD_OP);
 }
 
 lval eval_op(char* operator, lval x, lval y) {
-    if (x.type == LVAL_ERR) {
+    if (x.type == LVAL_ERROR) {
         return x;
     }
-    if (y.type == LVAL_ERR) {
+    if (y.type == LVAL_ERROR) {
         return y;
     }
 
     if (strcmp(operator, "+") == 0 || strcmp(operator, "add") == 0) {
-        return lval_num(x.value.num + y.value.num);
+        if (x.type == LVAL_DECIMAL && y.type == LVAL_DECIMAL) {
+            return lval_decimal(x.val.decimal + y.val.decimal);
+        } else if (x.type == LVAL_DECIMAL && y.type == LVAL_INTEGER) {
+            return lval_decimal(x.val.decimal + (double)y.val.integer);
+        } else if (x.type == LVAL_INTEGER && y.type == LVAL_DECIMAL) {
+            return lval_integer(x.val.integer + (int)y.val.decimal);
+        } else {
+            return lval_integer(x.val.integer + y.val.integer);
+        }
     }
     if (strcmp(operator, "-") == 0 || strcmp(operator, "sub") == 0) {
-        return lval_num(x.value.num - y.value.num);
+        if (x.type == LVAL_DECIMAL && y.type == LVAL_DECIMAL) {
+            return lval_decimal(x.val.decimal - y.val.decimal);
+        } else if (x.type == LVAL_DECIMAL && y.type == LVAL_INTEGER) {
+            return lval_decimal(x.val.decimal - y.val.integer);
+        } else if (x.type == LVAL_INTEGER && y.type == LVAL_DECIMAL) {
+            return lval_integer(x.val.integer - (int)y.val.decimal);
+        } else {
+            return lval_integer(x.val.integer - y.val.integer);
+        }
     }
     if (strcmp(operator, "*") == 0 || strcmp(operator, "mul") == 0) {
-        return lval_num(x.value.num * y.value.num);
+        if (x.type == LVAL_DECIMAL && y.type == LVAL_DECIMAL) {
+            return lval_decimal(x.val.decimal * y.val.decimal);
+        } else if (x.type == LVAL_DECIMAL && y.type == LVAL_INTEGER) {
+            return lval_decimal(x.val.decimal * y.val.integer);
+        } else if (x.type == LVAL_INTEGER && y.type == LVAL_DECIMAL) {
+            return lval_integer(x.val.integer * (int)y.val.decimal);
+        } else {
+            return lval_integer(x.val.integer * y.val.integer);
+        }
     }
     if (strcmp(operator, "/") == 0 || strcmp(operator, "div") == 0) {
-        return y.value.num == 0
-            ? lval_err(LERR_DIV_ZERO)
-            : lval_num(x.value.num / y.value.num);
+        if (x.type == LVAL_DECIMAL && y.type == LVAL_DECIMAL) {
+            return y.val.decimal == 0.0
+                ? lval_err(LERR_DIV_ZERO)
+                : lval_decimal(x.val.decimal / y.val.decimal);
+        } else if (x.type == LVAL_DECIMAL && y.type == LVAL_INTEGER) {
+            return y.val.integer == 0
+                ? lval_err(LERR_DIV_ZERO)
+                : lval_decimal(x.val.decimal / y.val.integer);
+        } else if (x.type == LVAL_INTEGER && y.type == LVAL_DECIMAL) {
+            return y.val.decimal == 0.0
+                ? lval_err(LERR_DIV_ZERO)
+                : lval_decimal(x.val.integer / (int)y.val.decimal);
+        } else {
+            return y.val.integer == 0
+                ? lval_err(LERR_DIV_ZERO)
+                : lval_decimal(x.val.integer / y.val.integer);
+        }
     }
     if (strcmp(operator, "%") == 0 || strcmp(operator, "mod") == 0) {
-        return lval_num(x.value.num % y.value.num);
+        if (x.type == LVAL_DECIMAL && y.type == LVAL_DECIMAL) {
+            return lval_decimal(fmod(x.val.decimal, y.val.decimal));
+        } else if (x.type == LVAL_DECIMAL && y.type == LVAL_INTEGER) {
+            return lval_decimal(fmod(x.val.decimal, y.val.integer));
+        } else if (x.type == LVAL_INTEGER && y.type == LVAL_DECIMAL) {
+            return lval_integer(fmod(x.val.integer, (int)y.val.decimal));
+        } else {
+            return lval_integer((int)fmod(x.val.integer, y.val.integer));
+        }
     }
     if (strcmp(operator, "^") == 0) {
-        return lval_num(pow(x.value.num, y.value.num));
+        if (x.type == LVAL_DECIMAL && y.type == LVAL_DECIMAL) {
+            return lval_decimal(pow(x.val.decimal, y.val.decimal));
+        } else if (x.type == LVAL_DECIMAL && y.type == LVAL_INTEGER) {
+            return lval_decimal(pow(x.val.decimal, y.val.integer));
+        } else if (x.type == LVAL_INTEGER && y.type == LVAL_DECIMAL) {
+            return lval_integer(pow(x.val.integer, y.val.decimal));
+        } else if (x.type == LVAL_INTEGER && y.type == LVAL_DECIMAL) {
+            return lval_integer(pow(x.val.integer, (int)y.val.decimal));
+        } else {
+            return lval_integer(pow(x.val.integer, y.val.integer));
+        }
     }
     if (strcmp(operator, "min") == 0) {
-        return x.value.num > y.value.num ? y : x;
+        if (x.type == LVAL_DECIMAL && y.type == LVAL_DECIMAL) {
+            return x.val.decimal < y.val.decimal ? x : y;
+        } else if (x.type == LVAL_DECIMAL && y.type == LVAL_INTEGER) {
+            return x.val.decimal < y.val.integer ? x : y;
+        } else if (x.type == LVAL_INTEGER && y.type == LVAL_DECIMAL) {
+            return x.val.integer < y.val.decimal ? x : y;
+        } else {
+            return x.val.integer < y.val.integer ? x : y;
+        }
     }
     if (strcmp(operator, "max") == 0) {
-        return x.value.num > y.value.num ? x : y;
+        if (x.type == LVAL_DECIMAL && y.type == LVAL_DECIMAL) {
+            return x.val.decimal > y.val.decimal ? x : y;
+        } else if (x.type == LVAL_DECIMAL && y.type == LVAL_INTEGER) {
+            return x.val.decimal > y.val.integer ? x : y;
+        } else if (x.type == LVAL_INTEGER && y.type == LVAL_DECIMAL) {
+            return x.val.integer > y.val.decimal ? x : y;
+        } else {
+            return x.val.integer > y.val.integer ? x : y;
+        }
+        return x.val.integer > y.val.integer ? x : y;
     }
 
     return lval_err(LERR_BAD_OP);
 }
 
 lval eval(mpc_ast_t* t) {
-    if (strstr(t->tag, "number")) {
+    if (strstr(t->tag, "integer")) {
         errno = 0;
         long x = strtol(t->contents, NULL, 10);
         return errno != ERANGE
-            ? lval_num(x)
+            ? lval_integer(x)
+            : lval_err(LERR_BAD_NUM);
+    }
+    if (strstr(t->tag, "decimal")) {
+        errno = 0;
+        double x = strtod(t->contents, NULL);
+        return errno != ERANGE
+            ? lval_decimal(x)
             : lval_err(LERR_BAD_NUM);
     }
 
@@ -136,6 +235,8 @@ lval eval(mpc_ast_t* t) {
 
 int main(int argc, char** argv) {
     mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Decimal = mpc_new("decimal");
+    mpc_parser_t* Integer = mpc_new("integer");
     mpc_parser_t* Operator = mpc_new("operator");
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Lliisspp = mpc_new("lispy");
@@ -151,7 +252,7 @@ int main(int argc, char** argv) {
 
     grammar[fsize] = 0;
 
-    mpca_lang(MPCA_LANG_DEFAULT, grammar, Number, Operator, Expr, Lliisspp);
+    mpca_lang(MPCA_LANG_DEFAULT, grammar, Number, Decimal, Integer,  Operator, Expr, Lliisspp);
 
     puts("lliisspp version 0.0.1");
     puts("Press Ctrl+C to Exit\n");
@@ -176,7 +277,7 @@ int main(int argc, char** argv) {
     }
 
     free(grammar);
-    mpc_cleanup(4, Number, Operator, Expr, Lliisspp);
+    mpc_cleanup(4, Number, Decimal, Integer, Operator, Expr, Lliisspp);
 
     return 0;
 }
